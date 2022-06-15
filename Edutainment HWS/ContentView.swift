f4a4378f7be504745f8035b91bde98a468ddc67c
NT <usedforfunny@gmail.com>
2022-06-15 20:19:05 +0200

    Initial Commit

---
+++ Edutainment HWS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edutainment HWS
+//
+//  Created by Alexandre Talatinian on 15/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edutainment HWS/Edutainment_HWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edutainment_HWSApp.swift
+//  Edutainment HWS
+//
+//  Created by Alexandre Talatinian on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Edutainment_HWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
